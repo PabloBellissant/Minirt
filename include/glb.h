e0ffe57d58e4ead480e8394dfaad2113fcfde2ea
PabloBellissant <adrilavaa@gmail.com>
2025-05-17 03:24:05 +0200

    Added json handler

---
+++ include/glb.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   glb.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabellis <mail@bellissantpablo.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 03:59:01 by pabellis          #+#    #+#             */
+/*   Updated: 2025/05/16 03:59:06 by pabellis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GLB_H
+# define GLB_H
+
+# include "struct.h"
+
+t_object	*glb_parser(char *str);
+t_object	get_object_data(int elem, char *str);
+t_json		*json_parser(char *json_text);
+
+#endif
