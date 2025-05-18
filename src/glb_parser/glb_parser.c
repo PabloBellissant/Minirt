603e8c3900a6bc0eb79fd1cc3fbe6425ccddff81
PabloBellissant <adrilavaa@gmail.com>
2025-05-18 02:51:21 +0200

    Better print debug

--- src/glb_parser/glb_parser.c
+++ src/glb_parser/glb_parser.c
@@ -25,7 +25,6 @@ t_object	*glb_parser(char *str)
 	int			i;
 	t_json		*json;
 
-	(void) str;
 	object_count = 2;
 	vector_init(&object_vector, sizeof(t_object));
 	json = json_parser(str);
@@ -40,14 +39,51 @@ t_object	*glb_parser(char *str)
 	return (0);
 }
 
+static void	print_array(t_json *json, int depth)
+{
+	int	i = 0;
+	while (i++ < depth)
+		dprintf(2, "    ");
+	if (json->type == JSON_NULL)
+		dprintf(2, "null\n");
+	else if (json->type == JSON_NUMBER)
+		dprintf(2, "%f\n", json->number);
+	else if (json->type == JSON_STRING)
+		dprintf(2, "%s\n", json->string);
+	else if (json->type == JSON_BOOL)
+		dprintf(2, "%s\n", json->boolean ? "true" : "false");
+	else if (json->type == JSON_ARRAY)
+		print_array(json, depth);
+	else if (json->type == JSON_OBJECT)
+		print_json(json->child, depth);
+	if (json->next)
+		print_array(json->next, depth);
+}
+
 static void	print_json(t_json *json, int depth)
 {
 	int	i = 0;
 	while (i++ < depth)
 		dprintf(2, "    ");
-	dprintf(2, "Key : %s, type : %d\n", json->key, json->type);
-	if (json->type == JSON_OBJECT)
+	dprintf(2, "%s : ", json->key);
+	if (json->type == JSON_NULL)
+		dprintf(2, "null\n");
+	else if (json->type == JSON_NUMBER)
+		dprintf(2, "%f\n", json->number);
+	else if (json->type == JSON_STRING)
+		dprintf(2, "%s\n", json->string);
+	else if (json->type == JSON_BOOL)
+		dprintf(2, "%s\n", json->boolean ? "true" : "false");
+	else if (json->type == JSON_ARRAY)
+	{
+		dprintf(2, "\n");
+		print_array(json->child, depth + 1);
+	}
+	else if (json->type == JSON_OBJECT)
+	{
+		dprintf(2, "\n");
 		print_json(json->child, depth + 1);
+	}
 	if (json->next)
 		print_json(json->next, depth);
 }
