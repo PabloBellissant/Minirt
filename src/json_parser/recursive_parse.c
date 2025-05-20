62a39abcc106a54dc4bb5ccc63079d95bf04c973
PabloBellissant <adrilavaa@gmail.com>
2025-05-21 01:02:37 +0200

    norm

--- src/json_parser/recursive_parse.c
+++ src/json_parser/recursive_parse.c
@@ -14,21 +14,25 @@
 #include "libft.h"
 #include "json.h"
 
+static t_json	*init_node(char *json_text);
+
 t_json	*recursive_parse(char *json_text)
 {
 	t_json	*json;
 	char	*next;
 	size_t	key_len;
 
-	json = malloc(sizeof(t_json));
-	if (!json)
-		return (NULL);
-	ft_bzero(json, sizeof(t_json));
 	if (*json_text == '{')
 		++json_text;
-	json->key = json_text;
+	json = init_node(json_text);
+	if (!json)
+		return (NULL);
 	key_len = extract_len(json_text);
-	fill_json_data(json, json_text + key_len + 1);
+	if (fill_json_data(json, json_text + key_len + 1) == -1)
+	{
+		free(json);
+		return (NULL);
+	}
 	next = get_next_data(json_text + key_len);
 	if (next)
 	{
@@ -40,5 +44,17 @@ t_json	*recursive_parse(char *json_text)
 		}
 	}
 	json->key[key_len] = '\0';
+	return (json);
+}
+
+static t_json	*init_node(char *json_text)
+{
+	t_json	*json;
+
+	json = malloc(sizeof(t_json));
+	if (!json)
+		return (NULL);
+	ft_bzero(json, sizeof(t_json));
+	json->key = json_text;
 	return (json);
 }
