27d90e34d2b534282280886e30ac04b5e2dad9e8
PabloBellissant <adrilavaa@gmail.com>
2025-05-26 07:11:31 +0200

    added define

--- include/define.h
+++ include/define.h
@@ -1,19 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   string.h                                           :+:      :+:    :+:   */
+/*   define.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabellis <mail@bellissantpablo.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 02:08:03 by pabellis          #+#    #+#             */
-/*   Updated: 2025/05/21 02:08:08 by pabellis         ###   ########.fr       */
+/*   Updated: 2025/05/23 05:30:26 by pabellis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRING_H
 # define STRING_H
 
-#define AUTHORS "'Aubry Richard Jaurel' And 'Bellissant Pablo'"
-#define TITLE "Mini rt by Pabellis and Jaubry--"
+# define SCREEN_WIDTH 1920
+# define SCREEN_HEIGHT 1080
+# define AUTHORS "'Aubry Richard Jaurel' And 'Bellissant Pablo'"
+# define TITLE "Mini rt by Pabellis and Jaubry--"
 
 #endif
