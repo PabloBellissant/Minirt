8f4cc2e22c0715ba3003e412010d028a192262b4
PabloBellissant <adrilavaa@gmail.com>
2025-05-26 07:13:08 +0200

    added a temp main

--- src/main.c
+++ src/main.c
@@ -10,13 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "libft.h"
-#include "glb.h"
 #include "parsing.h"
 #include "init.h"
+#include "loop.h"
+#include "threading.h"
+#include "define.h"
+
+# define SQUARE_SIZE 10
+
+int function(void *arg)
+{
+	t_task_data	*task_data;
+	t_data		*data;
+	int			i;
+
+	task_data = arg;
+	data = task_data->data;
+	i = 0;
+	while (i < SCREEN_WIDTH)
+	{
+		data->addr[i + (task_data->i * SCREEN_WIDTH)] = 0xFFFFFF;
+		usleep(1500);
+		++i;
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -25,10 +44,34 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		return (1);
-	if (parse_map(argv[1], &scene) != 0)
-		return (2);
 	if (init_graphics(&mlx_data) == -1)
-		return (-1); // free
+		return (2);
+	if (parse_map(argv[1], &scene) != 0)
+		return (3); //free mlx;
+
+	t_task	*task;
+	t_task_data	*data;
+	t_queue	queue;
+
+	ft_bzero(&queue, sizeof(t_queue));
+	queue.len = 0;
+	queue.start = 0;
+	init_threads(&queue);
+
+	int i = 0;
+	while (i < SCREEN_HEIGHT)
+	{
+		task = malloc(sizeof(t_task));
+		data = malloc(sizeof(t_task_data));
+		task->f = function;
+		data->data = &mlx_data;
+		data->i = i;
+		task->data = data;
+		enqueue(task, &queue);
+		++i;
+	}
+
+	loop_hook(&mlx_data);
 	(void) argv;
 	return (0);
 }
