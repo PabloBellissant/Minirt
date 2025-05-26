d8c754af46222524d625ff87727ca664d06878e7
PabloBellissant <adrilavaa@gmail.com>
2025-05-26 07:12:06 +0200

    added put_pixel

--- src/draw/put_pixel.c
+++ src/draw/put_pixel.c
@@ -1,3 +1,18 @@
-//
-// Created by pabellis on 5/23/25.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_pixel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabellis <mail@bellissantpablo.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 05:25:24 by pabellis          #+#    #+#             */
+/*   Updated: 2025/05/23 05:25:26 by pabellis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "define.h"
+
+inline void	put_pixel(int *addr, const int x, const int y, const int color)
+{
+	addr[y * SCREEN_WIDTH + x] = color;
+}
