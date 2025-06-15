314bf0a8494707ddc6d74392ccd9ffcfa0b0fda1
PabloBellissant <adrilavaa@gmail.com>
2025-06-15 08:10:34 +0200

    finished ambient

--- src/parsing/rt_parser/type/ambient.c
+++ src/parsing/rt_parser/type/ambient.c
@@ -19,25 +19,23 @@ static void	bad_ambient(int line_num);
 
 int	ambient(char *line, int actual_line, t_scene *scene)
 {
-	char	*arg;
-
-	arg = ft_strtok(line, " ");
-	arg = ft_strtok(NULL, " ");
-	if (arg == NULL)
+	line = ft_strtok(line, " ");
+	line = ft_strtok(NULL, " ");
+	if (line == NULL)
 	{
 		bad_ambient(actual_line);
 		return (-1);
 	}
-	scene->ambient.ratio = parse_float(arg, actual_line, 0, 1);
+	scene->ambient.ratio = parse_float(line, actual_line, 0, 1);
 	if (scene->ambient.ratio == NAN)
 		return (-1);
-	arg = ft_strtok(NULL, " ");
-	if (arg == NULL)
+	line = ft_strtok(NULL, "");
+	if (line == NULL)
 	{
 		bad_ambient(actual_line);
 		return (-1);
 	}
-	if (parse_color(arg, actual_line, &scene->ambient.color) == -1)
+	if (parse_color(line, actual_line, &scene->ambient.color) == -1)
 		return (-1);
 	return (0);
 }
@@ -49,20 +47,3 @@ static void	bad_ambient(int line_num)
 	ft_putnbr_fd(line_num, 2);
 	ft_putstr_fd("'\n", 2);
 }
-
-// int	ambient(char *line, int actual_line, t_scene *scene)
-// {
-// 	while (*line == ' ' || *line == '\t')
-// 		++line;
-// 	++line;
-// 	if (line[0] == '\0')
-// 		return (-1);
-// 	while (*line == ' ' || *line == '\t')
-// 		++line;
-// 	if (line[0] == '\0')
-// 		return (-1);
-// 	scene->ambient.ratio = parse_float(line, actual_line, 0, 1);
-// 	if (scene->ambient.ratio == NAN)
-// 		return (-1);
-// 	return (0);
-// }
