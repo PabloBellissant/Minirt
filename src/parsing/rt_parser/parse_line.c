e0970dea61cd3f9a66b753a6d09633636262ac9b
PabloBellissant <adrilavaa@gmail.com>
2025-06-30 07:37:47 +0200

    add rt parsing

--- src/parsing/rt_parser/parse_line.c
+++ src/parsing/rt_parser/parse_line.c
@@ -10,17 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "struct.h"
 #include "parsing.h"
 
 int	parse_line(t_scene *scene, char *line, int actual_line)
 {
 	t_object_type	type;
+	static int		(*f[])(char *, int, t_scene *) = {ambient, camera, light, sphere, plane, cylinder, obj};
 
 	if (line[0] == '#' || (line[0] == '/' && line[1] == '/') || line[0] == '\n')
 		return (0);
 	type = get_type(line, actual_line);
-	if (check_double(type, actual_line) == -1)
+	if (type == UNDEFINED || check_float(type, actual_line) == -1)
 		return (-1);
-	(void) scene;
+	f[type - 1](line, actual_line, scene);
 	return (0);
 }
