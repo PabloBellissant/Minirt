92448efa8427e1a69adcb5e493fafb0c4afec774
PabloBellissant <adrilavaa@gmail.com>
2025-07-07 05:56:26 +0200

    added function to glb.h

--- include/glb.h
+++ include/glb.h
@@ -14,8 +14,16 @@
 # define GLB_H
 
 # include "struct.h"
+# include "json.h"
 
-t_object	*glb_parser(char *str);
-t_object	get_object_data(int elem, char *str);
+t_obj		*glb_parser(char *str);
+t_obj		get_object_data(int elem, char *str);
+char		*get_scene_name(t_json *json);
+int			get_object_count(t_json *json);
+int			get_vertex_count(t_json *json, int accessors_id);
+t_obj		*parse_objects(t_json *json, void *binary);
+int			get_mesh_id(t_json *json, int object_id);
+int			get_pos_accessors(t_json *json, int object_id);
+t_vertex	*get_vertex(t_json *json, void *binary, int id);
 
 #endif
