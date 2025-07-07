faff4588272df4f610d73b9f380c14e0ec8acfdc
PabloBellissant <adrilavaa@gmail.com>
2025-07-07 05:49:45 +0200

    added a bounce max define

--- include/define.h
+++ include/define.h
@@ -15,6 +15,7 @@
 
 # define SCREEN_WIDTH 1920
 # define SCREEN_HEIGHT 1080
+# define BOUNCE_MAX 1000
 # define AUTHORS "'Aubry Richard Jaurel' And 'Bellissant Pablo'"
 # define TITLE "Mini rt by Pabellis and Jaubry--"
 
