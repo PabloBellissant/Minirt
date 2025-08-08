650c23f6a4385122e676cb1898cb3b8658142b74
PabloBellissant <adrilavaa@gmail.com>
2025-08-08 03:28:46 +0200

    add hit_reg_plane

--- include/calc.h
+++ include/calc.h
@@ -17,8 +17,9 @@
 # include "render.h"
 
 t_rgb_int	ray_path(t_ray *ray, t_scene *scene);
-float		hit_register(t_ray *ray, t_vector *obj_vec);
-int			hit_plane(t_ray *ray, t_object *o, float *t);
+float		hit_register(t_ray *restrict ray, t_scene *scene);
+t_object	*hit_reg_plane(t_ray *ray, t_scene *scene);
+int			hit_plane(t_ray *ray, t_plane *p, float *t);
 int			hit_sphere(t_ray *ray, t_object *o, float *t);
 int			hit_cylinder(t_ray *ray, t_object *o, float *t);
 
