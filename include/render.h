ebe368bd3b8f9afd53d5e309476afb08f24d2db2
PabloBellissant <adrilavaa@gmail.com>
2025-09-04 00:31:07 +0200

    rename functions

--- include/render.h
+++ include/render.h
@@ -76,7 +76,8 @@ typedef struct s_scene
 	t_bvh		*bvh;
 }				t_scene;
 
-int		rasterize_bvh(t_bvh *bvh, t_img_data *img, t_camera *camera);
-t_vec2i	projection_matrix(t_vec3 *p, t_camera *camera);
+int		rasterize_cuboid(t_cuboid *c, t_img_data *img,
+	t_camera *camera, int color);
+t_vec2i	project_point(t_vec3 *p, t_camera *camera);
 
 #endif
