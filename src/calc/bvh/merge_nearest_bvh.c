a1107bb406fb8bfe7a5b2e76182fdae003f63100
PabloBellissant <adrilavaa@gmail.com>
2025-09-03 03:52:14 +0200

    fix bvh for far objects

--- src/calc/bvh/merge_nearest_bvh.c
+++ src/calc/bvh/merge_nearest_bvh.c
@@ -13,39 +13,38 @@
 #include "bvh.h"
 #include <float.h>
 
-size_t	get_next_no_parent(ssize_t actual, t_bvh *bvh, ssize_t max);
-
 void	merge_nearest_bvh(t_vector *bvh_vec)
 {
 	size_t	best[2];
-	size_t	i_old[2];
+	size_t	i[2];
 	float	min_area;
 	float	actual_area;
 	t_bvh	*bvh;
 
 	bvh = bvh_vec->data;
 	min_area = FLT_MAX;
-	i_old[0] = get_next_no_parent(-1, bvh, bvh_vec->num_elements - 1);
-	i_old[1] = get_next_no_parent(i_old[0], bvh, bvh_vec->num_elements - 1);
-	while (i_old[1] < bvh_vec->num_elements)
+	i[0] = 0;
+	while (i[0] < bvh_vec->num_elements)
 	{
-		actual_area = get_next_bvh_area(&bvh[i_old[0]], &bvh[i_old[1]]);
-		if (actual_area < min_area)
+		if (bvh[i[0]].parent == NULL)
 		{
-			min_area = actual_area;
-			best[0] = i_old[0];
-			best[1] = i_old[1];
+			i[1] = 0;
+			while (i[1] < bvh_vec->num_elements)
+			{
+				if (bvh[i[1]].parent == NULL && i[0] != i[1])
+				{
+					actual_area = get_next_bvh_area(&bvh[i[0]], &bvh[i[1]]);
+					if (actual_area < min_area)
+					{
+						min_area = actual_area;
+						best[0] = i[0];
+						best[1] = i[1];
+					}
+				}
+				++i[1];
+			}
 		}
-		i_old[0] = i_old[1];
-		i_old[1] = get_next_no_parent(i_old[1], bvh, bvh_vec->num_elements - 1);
+		++i[0];
 	}
 	merge_bvh(bvh_vec, best[0], best[1]);
-}
-
-size_t	get_next_no_parent(ssize_t actual, t_bvh *bvh, ssize_t max)
-{
-	++actual;
-	while ((actual < max) && (bvh[actual].parent != NULL))
-		++actual;
-	return (actual);
 }
