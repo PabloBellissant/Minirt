d8cbeaec8dd441149fc14e6ce4fc060cbf726e49
PabloBellissant <adrilavaa@gmail.com>
2025-09-03 03:51:45 +0200

    added rasterization folder

--- src/calc/rasterization/rasterization.mk
+++ src/calc/rasterization/rasterization.mk
@@ -1,30 +1,22 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    calc.mk                                            :+:      :+:    :+:    #
+#    rasterization.mk                                   :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/07/22 18:39:51 by jaubry--          #+#    #+#              #
-#    Updated: 2025/08/05 01:52:31 by jaubry--         ###   ########lyon.fr    #
+#    Created: 2025/07/22 18:41:28 by jaubry--          #+#    #+#              #
+#    Updated: 2025/08/04 23:24:24 by jaubry--         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 # Directories
-CALC_DIR	= $(SRCDIR)/calc
+RASTERIZATION_DIR	= $(CALC_DIR)/rasterization
 
 # Source files
-CALC_SRCS	= hit_register.c \
-			  hit_reg_plane.c \
-			  ray_path.c \
-			  phong_reflection.c
-SRCS		+= $(addprefix $(CALC_DIR)/, $(CALC_SRCS))
-
-CALC_MKS	= bvh/bvh.mk \
-			  objects/objects.mk \
-			  rasterization/rasterization.mk
-
-include $(addprefix $(CALC_DIR)/, $(CALC_MKS))
+RASTERIZATION_SRCS	= rasterize_bvh.c \
+					  projection_matrix.c
+SRCS				+= $(addprefix $(RASTERIZATION_DIR)/, $(RASTERIZATION_SRCS))
 
 # VPATH
-vpath %.c $(CALC_DIR)
+vpath %.c $(RASTERIZATION_DIR)
