af134577795470a00439db089c3cad22a60d0db2
PabloBellissant <adrilavaa@gmail.com>
2025-09-04 02:36:43 +0200

    added rasterize bvh header

--- include/render.h
+++ include/render.h
@@ -76,8 +76,11 @@ typedef struct s_scene
 	t_bvh		*bvh;
 }				t_scene;
 
+typedef struct s_data t_data;
+
 int		rasterize_cuboid(t_cuboid *c, t_img_data *img,
 	t_camera *camera, int color);
+void	rasterize_bvh(t_bvh *bvh, int depth, int total_depth, t_data *data);
 t_vec2i	project_point(t_vec3 *p, t_camera *camera);
 
 #endif
