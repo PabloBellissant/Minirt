1c74c5e18478e2b54c00b891ca1ebc1d1ac93961
PabloBellissant <adrilavaa@gmail.com>
2025-09-07 03:57:39 +0200

    remove useless functions

--- src/calc/bvh/bvh.mk
+++ src/calc/bvh/bvh.mk
@@ -15,10 +15,11 @@ BVH_DIR		= $(CALC_DIR)/bvh
 
 # Source files
 BVH_SRCS	= create_bvh.c \
-			  get_bvh_area.c get_next_bvh_area.c \
-			  get_object_count.c \
-			  merge_bvh.c merge_nearest_bvh.c \
-			  hit_bvh.c hit_box_bvh.c
+			  get_next_bvh_area.c \
+			  merge_bvh.c \
+			  merge_nearest_bvh.c \
+			  hit_bvh.c \
+			  hit_box_bvh.c
 SRCS		+= $(addprefix $(BVH_DIR)/, $(BVH_SRCS))
 
 # VPATH
