14f19403f35a5cd47a03215820fe14ada40ba56e
PabloBellissant <adrilavaa@gmail.com>
2025-09-07 03:58:31 +0200

    remove the section parent in t_bvh

--- src/calc/bvh/merge_nearest_bvh.c
+++ src/calc/bvh/merge_nearest_bvh.c
@@ -13,7 +13,7 @@
 #include "bvh.h"
 #include <float.h>
 
-void	merge_nearest_bvh(t_vector *bvh_vec)
+void	merge_nearest_bvh(t_vector *bvh_vec, int *parents)
 {
 	size_t	best[2];
 	size_t	i[2];
@@ -26,12 +26,12 @@ void	merge_nearest_bvh(t_vector *bvh_vec)
 	i[0] = 0;
 	while (i[0] < bvh_vec->num_elements)
 	{
-		if (bvh[i[0]].parent == NULL)
+		if (parents[i[0]] == 0)
 		{
 			i[1] = i[0] + 1;
 			while (i[1] < bvh_vec->num_elements)
 			{
-				if (bvh[i[1]].parent == NULL)
+				if (parents[i[1]] == 0)
 				{
 					actual_area = get_next_bvh_area(&bvh[i[0]], &bvh[i[1]]);
 					if (actual_area < min_area)
@@ -47,4 +47,6 @@ void	merge_nearest_bvh(t_vector *bvh_vec)
 		++i[0];
 	}
 	merge_bvh(bvh_vec, best[0], best[1]);
+	parents[best[0]] = 1;
+	parents[best[1]] = 1;
 }
