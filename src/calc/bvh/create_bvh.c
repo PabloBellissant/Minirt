c8bb0b00c7874e5a65bdc57f3b1c34542319d9c4
PabloBellissant <adrilavaa@gmail.com>
2025-09-07 03:53:25 +0200

    change parent in bvh by parent_list

--- src/calc/bvh/create_bvh.c
+++ src/calc/bvh/create_bvh.c
@@ -16,9 +16,9 @@
 #include "render.h"
 
 static int		create_object_bvh(t_scene *scene, t_vector *bvh_vec);
-static void		set_bvh_size(t_object *object, t_vec3 *pos, t_vec3 *size);
-bool			is_bvh_full(t_vector *bvh_vec);
-t_bvh			*get_root(t_vector *bvh_vec);
+static void		set_bvh_size(t_object *object, t_vec3 *pos, t_vec3 *max);
+bool			is_bvh_full(t_vector *bvh_vec, int *parents);
+t_bvh			*get_root(t_vector *bvh_vec, int *parents);
 
 int	init_bvh(t_vector *bvh_vec, t_scene *scene)
 {
@@ -50,40 +50,45 @@ int	init_bvh(t_vector *bvh_vec, t_scene *scene)
 int	create_bvh(t_scene *scene)
 {
 	t_vector	bvh_vec;
+	int			*parents;
 
 	if (init_bvh(&bvh_vec, scene) == -1)
 		return (-1);
+	parents = ft_calloc(bvh_vec.max_elements, sizeof(int));
+	if (!parents)
+	{
+		free_vector(&bvh_vec);
+		return (-1);
+	}
 	create_object_bvh(scene, &bvh_vec);
-	while (is_bvh_full(&bvh_vec) == false)
-		merge_nearest_bvh(&bvh_vec);
-	scene->bvh = get_root(&bvh_vec);
+	while (is_bvh_full(&bvh_vec, parents) == false)
+		merge_nearest_bvh(&bvh_vec, parents);
+	scene->bvh = get_root(&bvh_vec, parents);
 	return (0);
 }
 
-t_bvh	*get_root(t_vector *bvh_vec)
+t_bvh	*get_root(t_vector *bvh_vec, int *parents)
 {
-	size_t	i;
-	t_bvh	*bvh;
+	size_t		i;
+	t_bvh		*bvh;
 
 	bvh = bvh_vec->data;
 	i = 0;
-	while (bvh[i].parent != NULL)
+	while (parents[i] == 1)
 		++i;
 	return (&bvh[i]);
 }
 
-bool	is_bvh_full(t_vector *bvh_vec)
+bool	is_bvh_full(t_vector *bvh_vec, int *parents)
 {
 	size_t	i;
-	t_bvh	*bvh;
 	int		orphin_count;
 
-	bvh = bvh_vec->data;
 	orphin_count = 0;
 	i = 0;
 	while (i < bvh_vec->num_elements)
 	{
-		if (bvh[i].parent == NULL)
+		if (parents[i] == 0)
 			++orphin_count;
 		if (orphin_count == 2)
 			return (false);
@@ -105,7 +110,7 @@ static int	create_object_bvh(t_scene *scene, t_vector *bvh_vec)
 	{
 		if (object[i].type != PLANE && object[i].type != LIGHT)
 		{
-			set_bvh_size(&(object[i]), &single_bvh.pos, &single_bvh.size);
+			set_bvh_size(&(object[i]), &single_bvh.min, &single_bvh.max);
 			single_bvh.object = &(object[i]);
 			vector_add(bvh_vec, &single_bvh, 1);
 		}
@@ -114,15 +119,15 @@ static int	create_object_bvh(t_scene *scene, t_vector *bvh_vec)
 	return (0);
 }
 
-static void	set_bvh_size(t_object *object, t_vec3 *pos, t_vec3 *size)
+static void	set_bvh_size(t_object *object, t_vec3 *pos, t_vec3 *max)
 {
 	if (object->type == SPHERE)
 	{
 		pos->x = object->sphere.pos.x - (object->sphere.diameter / 2);
 		pos->y = object->sphere.pos.y - (object->sphere.diameter / 2);
 		pos->z = object->sphere.pos.z - (object->sphere.diameter / 2);
-		size->x = object->sphere.diameter;
-		size->y = object->sphere.diameter;
-		size->z = object->sphere.diameter;
+		max->x = pos->x + object->sphere.diameter;
+		max->y = pos->y + object->sphere.diameter;
+		max->z = pos->z + object->sphere.diameter;
 	}
 }
