a74e72d7a0dee197d0ac716d48b26517f169c5fa
PabloBellissant <adrilavaa@gmail.com>
2025-09-08 04:09:25 +0200

    added code for cylinder

--- src/calc/hit_register.c
+++ src/calc/hit_register.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   hit_register.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/08 02:48:48 by pabellis          #+#    #+#             */
-/*   Updated: 2025/08/19 23:37:29 by pabellis         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   hit_register.c									 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: pabellis <pabellis@student.forty2.fr>	  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2025/08/08 02:48:48 by pabellis		  #+#	#+#			 */
+/*   Updated: 2025/08/19 23:37:29 by pabellis		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include <float.h>
@@ -35,10 +35,17 @@ static inline void	hit_register_obj(t_ray *restrict ray,
 		ray->hit_normal = objects->plane.normal;
 		ray->hit_rgb = objects->plane.rgb;
 	}
-	// else
-	// {
-	//		code for cylinder
-	// }
+	else if (objects->type == CYLINDER)
+	{
+		t_vec3	axis = objects->cylinder.rot;
+		t_vec3	to_hit;
+		to_hit = vec3_sub(hit_point, objects->cylinder.pos);
+		axis = vec3_scale(axis, vec3_dot(to_hit, axis));
+		axis = vec3_add(objects->cylinder.pos, axis);
+		ray->hit_normal = vec3_sub(hit_point, axis);
+		ray->hit_normal = unsafe_vec3_normalize(ray->hit_normal);
+		ray->hit_rgb = objects->cylinder.rgb;
+	}
 	ray->pos = hit_point;
 }
 
