caef92b9756b00e74f0a8c6e68706863ac9b93eb
PabloBellissant <adrilavaa@gmail.com>
2025-09-11 03:49:43 +0200

    added .mk

--- src/calc/bvh/aabb/aabb.mk
+++ src/calc/bvh/aabb/aabb.mk
@@ -1,27 +1,25 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    bvh.mk                                             :+:      :+:    :+:    #
+#    aabb.mk                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+         #
+#    By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/07/22 18:41:28 by jaubry--          #+#    #+#              #
-#    Updated: 2025/08/04 23:24:24 by jaubry--         ###   ########lyon.fr    #
+#    Created: 2025/07/22 18:41:28 by pabellis          #+#    #+#              #
+#    Updated: 2025/08/04 23:24:24 by pabellis         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 # Directories
-BVH_DIR		= $(CALC_DIR)/bvh
+AABB_DIR		= $(BVH_DIR)/aabb
 
 # Source files
-BVH_SRCS	= hit_bvh.c \
-			  hit_box_bvh.c
+AABB_SRCS	= create_bvh.c \
+			  get_next_bvh_area.c \
+			  merge_bvh.c \
+			  merge_nearest_bvh.c
 
-SRCS		+= $(addprefix $(BVH_DIR)/, $(BVH_SRCS))
-
-BVH_MKS	= aabb/aabb.mk
-
-include $(addprefix $(BVH_DIR)/, $(BVH_MKS))
+SRCS		+= $(addprefix $(AABB_DIR)/, $(AABB_SRCS))
 
 # VPATH
-vpath %.c $(BVH_DIR)
+vpath %.c $(AABB_DIR)
