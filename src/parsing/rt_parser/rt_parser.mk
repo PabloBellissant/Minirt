c4ee6aa3cf605271201f9e6ea24185a154750744
PabloBellissant <adrilavaa@gmail.com>
2025-09-11 03:51:29 +0200

    norm

--- src/parsing/rt_parser/rt_parser.mk
+++ src/parsing/rt_parser/rt_parser.mk
@@ -3,7 +3,7 @@
 #                                                         :::      ::::::::    #
 #    rt_parser.mk                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jaubry-- <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/22 19:25:14 by jaubry--          #+#    #+#              #
 #    Updated: 2025/07/22 21:55:38 by jaubry--         ###   ########.fr        #
