3e72cfd05f07c01d707f92bb3ce8353b9afee8ce
PabloBellissant <adrilavaa@gmail.com>
2025-09-12 06:23:44 +0200

    added mk

---
+++ src/calc/bvh/sphere/sphere.mk
@@ -0,0 +1,27 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    sphere.mk                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/07/22 18:41:28 by pabellis          #+#    #+#              #
+#    Updated: 2025/08/04 23:24:24 by pabellis         ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+# Directories
+SPHERE_DIR		= $(BVH_DIR)/sphere
+
+# Source files
+SPHERE_SRCS	= create_sphere_bvh.c \
+			  merge_nearest_sphere_bvh.c \
+			  get_next_sphere_bvh_area.c \
+			  merge_sphere_bvh.c \
+			  hit_sphere_bvh.c \
+			  get_sphere_root.c
+
+SRCS		+= $(addprefix $(SPHERE_DIR)/, $(SPHERE_SRCS))
+
+# VPATH
+vpath %.c $(SPHERE_DIR)
