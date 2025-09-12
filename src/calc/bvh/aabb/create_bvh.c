efdf74e9f4a77bfb87019d30bbf53aea47f913a6
PabloBellissant <adrilavaa@gmail.com>
2025-09-12 06:24:14 +0200

    move code to get_bvh_count

--- src/calc/bvh/aabb/create_bvh.c
+++ src/calc/bvh/aabb/create_bvh.c
@@ -17,42 +17,24 @@
 
 static int		create_object_bvh(t_scene *scene, t_vector *bvh_vec);
 static void		set_bvh_size(t_object *object, t_vec3 *min, t_vec3 *max);
-bool			is_bvh_full(t_vector *bvh_vec, int *parents);
-t_bvh			*get_root(t_vector *bvh_vec, int *parents);
 
-int	init_bvh(t_vector *bvh_vec, t_scene *scene)
+int	init_aabb_bvh(t_vector *bvh_vec, t_scene *scene)
 {
 	size_t		volume_count;
-	size_t		i;
-	t_object	*object;
 
-	vector_init(bvh_vec, sizeof(t_bvh));
-	object = scene->objects.data;
-	volume_count = 0;
-	i = 0;
-	while (i < scene->objects.num_elements)
-	{
-		if (object[i].type != PLANE && object[i].type != LIGHT)
-			++volume_count;
-		++i;
-	}
-	i = volume_count;
-	while (i > 1)
-	{
-		volume_count += i / 2;
-		i -= i / 2;
-	}
+	vector_init(bvh_vec, sizeof(t_aabb_bvh));
+	volume_count = get_bvh_count(&scene->objects);
 	if (set_vector_size(bvh_vec, volume_count) != 0)
 		return (-1);
 	return (0);
 }
 
-int	create_bvh(t_scene *scene)
+int	create_aabb_bvh(t_scene *scene)
 {
 	t_vector	bvh_vec;
 	int			*parents;
 
-	if (init_bvh(&bvh_vec, scene) == -1)
+	if (init_aabb_bvh(&bvh_vec, scene) == -1)
 		return (-1);
 	parents = ft_calloc(bvh_vec.max_elements, sizeof(int));
 	if (!parents)
@@ -63,51 +45,20 @@ int	create_bvh(t_scene *scene)
 	create_object_bvh(scene, &bvh_vec);
 	while (is_bvh_full(&bvh_vec, parents) == false)
 		merge_nearest_bvh(&bvh_vec, parents);
-	scene->bvh = get_root(&bvh_vec, parents);
+	scene->bvh.aabb_bvh = get_aabb_root(&bvh_vec, parents);
 	free(parents);
 	return (0);
 }
 
-t_bvh	*get_root(t_vector *bvh_vec, int *parents)
-{
-	size_t		i;
-	t_bvh		*bvh;
-
-	bvh = bvh_vec->data;
-	i = 0;
-	while (parents[i] == 1)
-		++i;
-	return (&bvh[i]);
-}
-
-bool	is_bvh_full(t_vector *bvh_vec, int *parents)
-{
-	size_t	i;
-	int		orphin_count;
-
-	orphin_count = 0;
-	i = 0;
-	while (i < bvh_vec->num_elements)
-	{
-		if (parents[i] == 0)
-			++orphin_count;
-		if (orphin_count == 2)
-			return (false);
-		++i;
-	}
-	return (true);
-}
-
 static int	create_object_bvh(t_scene *scene, t_vector *bvh_vec)
 {
-	t_bvh		single_bvh;
+	t_aabb_bvh		single_bvh;
 	t_object	*object;
 	size_t		i;
 
-	ft_bzero(&single_bvh, sizeof(t_bvh));
+	ft_bzero(&single_bvh, sizeof(t_aabb_bvh));
 	object = scene->objects.data;
 	i = 0;
-	single_bvh.rotation = (t_vec3){{0, 0, 0}};
 	while (i < scene->objects.num_elements)
 	{
 		if (object[i].type != PLANE && object[i].type != LIGHT)
---
+++ src/calc/bvh/get_bvh_count.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_bvh_count.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 04:53:45 by pabellis          #+#    #+#             */
+/*   Updated: 2025/09/12 04:53:48 by pabellis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stddef.h"
+#include "minirt.h"
+
+size_t	get_bvh_count(t_vector *objects)
+{
+	size_t		volume_count;
+	size_t		i;
+	t_object	*object;
+
+	object = objects->data;
+	volume_count = 0;
+	i = 0;
+	while (i < objects->num_elements)
+	{
+		if (object[i].type != PLANE && object[i].type != LIGHT)
+			++volume_count;
+		++i;
+	}
+	i = volume_count;
+	while (i > 1)
+	{
+		volume_count += i / 2;
+		i -= i / 2;
+	}
+	return (volume_count);
+}
