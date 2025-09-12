75a6941cc5ef24dd8ccd65948602d879862d3f0d
PabloBellissant <adrilavaa@gmail.com>
2025-09-12 06:25:19 +0200

    move to another file

---
+++ src/calc/bvh/aabb/get_root.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_root.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 05:00:12 by pabellis          #+#    #+#             */
+/*   Updated: 2025/09/12 05:00:14 by pabellis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "bvh.h"
+
+t_aabb_bvh	*get_aabb_root(t_vector *bvh_vec, int *parents)
+{
+	size_t		i;
+	t_aabb_bvh	*bvh;
+
+	bvh = bvh_vec->data;
+	i = 0;
+	while (parents[i] == 1)
+		++i;
+	return (&bvh[i]);
+}
