cfd495177ac9514efbcadb392a3fac92308e638a
pabellis <mail@bellissantpablo.fr>
2025-09-27 01:38:02 +0200

    add hit_plane.c

---
+++ src/calc/objects/hit_plane.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hit_plane.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 02:22:51 by pabellis          #+#    #+#             */
+/*   Updated: 2025/09/23 02:23:02 by pabellis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "calc.h"
+#include <float.h>
+
+int	hit_plane(t_ray *restrict ray, t_object *restrict o, float *t);
+
+inline int	hit_plane(t_ray *restrict ray, t_object *restrict o, float *t)
+{
+	float	denom;
+
+	denom = vec3_dot(ray->dir, o->plane.normal) + FLT_MIN;
+	*t = vec3_dot(vec3_sub(o->plane.pos, ray->pos), o->plane.normal) / denom;
+	return (*t >= 0);
+}
