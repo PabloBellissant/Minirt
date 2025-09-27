0578db207a442615dcf64de33a4e1e193d4e5b20
pabellis <mail@bellissantpablo.fr>
2025-09-27 04:40:54 +0200

    added hit_plane

--- src/calc/objects/objects.mk
+++ src/calc/objects/objects.mk
@@ -15,7 +15,8 @@ OBJECTS_DIR		= $(CALC_DIR)/objects
 
 # Source files
 OBJECTS_SRCS	= hit_cylinder.c \
-				  hit_sphere.c
+				  hit_sphere.c \
+				  hit_plane.c
 SRCS			+= $(addprefix $(OBJECTS_DIR)/, $(OBJECTS_SRCS))
 
 # VPATH
