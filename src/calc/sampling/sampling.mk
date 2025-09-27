93a9ea72f98fa581fc34694c8f4364581a1cf05f
pabellis <mail@bellissantpablo.fr>
2025-09-27 04:57:28 +0200

    added sampling directory

---
+++ src/calc/sampling/sampling.mk
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    bound.mk                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/07/22 18:31:40 by pabellis          #+#    #+#              #
+#    Updated: 2025/07/22 18:37:04 by pabellis         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Directories
+SAMPLING_DIR	= $(CALC_DIR)/sampling
+
+# Source files
+SAMPLING_SRCS	= draw_supersampling.c
+
+SRCS		+= $(addprefix $(SAMPLING_DIR)/, $(SAMPLING_SRCS))
+
+# VPATH
+vpath %.c $(SAMPLING_DIR)
