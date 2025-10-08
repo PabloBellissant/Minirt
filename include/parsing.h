32a7a8eff9b3dff05d9b7f0260161f620d61765e
pabellis <mail@bellissantpablo.fr>
2025-10-08 05:51:08 +0200

    added const to char *line

--- include/parsing.h
+++ include/parsing.h
@@ -32,17 +32,17 @@ typedef enum e_object_type
 int				parse_scene(char *file_name, t_scene *scene);
 int				rt_parser(int fd, t_scene *scene);
 int				fill_by_type(t_scene *scene);
-int				parse_line(t_scene *scene, char *line, int actual_line);
-t_object_type	get_type(char *line, int actual_line);
+int				parse_line(t_scene *scene, const char *line, int actual_line);
+t_object_type	get_type(const char *line, int actual_line);
 int				check_float(t_object_type type, int actual_line);
 t_object		*create_object(t_scene *scene, t_object_type type);
 
-int				ambient(char *line, int line_num, t_scene *scene);
-int				camera(char *line, int line_num, t_scene *scene);
-int				light(char *line, int line_num, t_scene *scene);
-int				sphere(char *line, int line_num, t_scene *scene);
-int				plane(char *line, int line_num, t_scene *scene);
-int				cylinder(char *line, int line_num, t_scene *scene);
-int				obj(char *line, int line_num, t_scene *scene);
+int				ambient(const char *line, int line_num, t_scene *scene);
+int				camera(const char *line, int line_num, t_scene *scene);
+int				light(const char *line, int line_num, t_scene *scene);
+int				sphere(const char *line, int line_num, t_scene *scene);
+int				plane(const char *line, int line_num, t_scene *scene);
+int				cylinder(const char *line, int line_num, t_scene *scene);
+int				obj(const char *line, int line_num, t_scene *scene);
 
 #endif
