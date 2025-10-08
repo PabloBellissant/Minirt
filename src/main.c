fef29798159e2cc74841638871a9c17dbc5df29d
pabellis <mail@bellissantpablo.fr>
2025-10-08 06:01:06 +0200

    remove comment code

--- src/main.c
+++ src/main.c
@@ -73,7 +73,7 @@ int	main(int argc, char **argv)
 	char *tempstr = strdup("asset/pabellis.xpm");
 	data.scene.tex = mlx_xpm_file_to_image(data.mlx->mlx, tempstr, &data.scene.tex_width, &data.scene.tex_height);
 	data.scene.pixels = mlx_get_data_addr(data.scene.tex, &data.scene.tex_bpp, &data.scene.tex_size_line, &data.scene.tex_endian);
-	//loop_hook(&data);
+	loop_hook(&data);
 	free_data(data);
 	kill_threads(data.queue, NPROC);
 	return (0);
