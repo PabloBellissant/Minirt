01f9804c773a57a6906e31163bb1889714ae3f6d
pabellis <mail@bellissantpablo.fr>
2025-10-12 07:22:02 +0200

    remove temp parsing

--- src/main.c
+++ src/main.c
@@ -68,13 +68,11 @@ int	main(int argc, char **argv)
 	data.queue = &queue;
 	if (init_graphics(&data) == -1)
 		return (2);
+	data.scene.mlx = data.mlx;
 	if (parse_scene(argv[1], &data.scene) != 0)
 		return (3); //free mlx;
 	init_threads(data.queue);
 	data.params.bvh_depth = data.scene.bvh.aabb_bvh->depth;
-	char *tempstr = strdup("asset/pabellis.xpm");
-	data.scene.tex = mlx_xpm_file_to_image(data.mlx->mlx, tempstr, &data.scene.tex_width, &data.scene.tex_height);
-	data.scene.pixels = mlx_get_data_addr(data.scene.tex, &data.scene.tex_bpp, &data.scene.tex_size_line, &data.scene.tex_endian);
 	loop_hook(&data);
 	free_data(data);
 	kill_threads(data.queue, NPROC);
