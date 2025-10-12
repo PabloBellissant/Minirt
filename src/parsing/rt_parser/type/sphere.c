4e7666cbeaa006222248405a8e835ef227e29b04
pabellis <mail@bellissantpablo.fr>
2025-10-12 07:20:13 +0200

    added texture to sphere

--- src/parsing/rt_parser/type/sphere.c
+++ src/parsing/rt_parser/type/sphere.c
@@ -15,7 +15,7 @@
 #include "vectors.h"
 
 #define SPHERE_FORMAT " *sp  *%f *, *%f *, *%f  *%f  *%8[255] *\
-, *%8[255] *, *%8[255] *\n"
+, *%8[255] *, *%8[255](  *%s) *\n"
 
 int	sphere(const char *line, int line_num, t_scene *scene)
 {
@@ -23,16 +23,25 @@ int	sphere(const char *line, int line_num, t_scene *scene)
 	t_vec3		*pos;
 	t_rgb_int	color;
 	t_object	*object;
-	float		radius;
+	char		*texture_name;
 
 	object = create_object(scene, SPHERE);
 	sphere = &object->sphere;
 	pos = &sphere->pos;
 	if (ft_scan(line_num, SPHERE_FORMAT, line, &pos->x, &pos->y, &pos->z,
-			&sphere->diameter, &color.r, &color.g, &color.b))
+			&sphere->diameter, &color.r, &color.g, &color.b, &texture_name))
 		return (-1);
-	radius = sphere->diameter / 2.0f;
-	sphere->radius_squared = radius * radius;
+	if (*texture_name != 0)
+	{
+		object->texture = get_texture(scene, texture_name);
+		if (!object->texture)
+		{
+			ft_dprintf(2, "Error\nCannot find texture '%s', line : '%d'\n", texture_name, line_num);
+			return (-1);
+		}
+	}
+	sphere->radius_squared = (sphere->diameter / 2.0f)
+		* (sphere->diameter / 2.0f);
 	object->f = hit_sphere;
 	sphere->rgb = rgb_itof(color);
 	return (0);
