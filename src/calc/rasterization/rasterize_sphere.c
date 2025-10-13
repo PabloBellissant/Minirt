9048ab631f9decc2be776478cf8601d32554141d
pabellis <mail@bellissantpablo.fr>
2025-10-13 03:00:26 +0200

    struct name change

--- src/calc/rasterization/rasterize_sphere.c
+++ src/calc/rasterization/rasterize_sphere.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include <math.h>
-#include "minirt.h"
+#include "calc.h"
 #include "render.h"
 #include "mlx_wrapper.h"
 
