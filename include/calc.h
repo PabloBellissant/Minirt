b240f9493db0589d97c7972c3068f58a87709758
pabellis <mail@bellissantpablo.fr>
2025-10-13 03:49:34 +0200

    create s_quadratic structure

--- include/calc.h
+++ include/calc.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calc.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: pabellis <pabellis@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/25 17:50:50 by pabellis          #+#    #+#             */
-/*   Updated: 2025/08/07 09:38:09 by jaubry--         ###   ########.fr       */
+/*   Created: 2025/10/12 07:15:58 by pabellis          #+#    #+#             */
+/*   Updated: 2025/10/13 02:22:01 by pabellis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 # include "object.h"
 # include "render.h"
+
+# define EPSILON 1e-6f
+# define M_PIf 3.14159265358979323846f
+
+typedef struct s_quadratic
+{
+	float	a;
+	float	b;
+	float	c;
+	float	delta;
+	float	t_min;
+	float	t_max;
+	float	oo;
+	float	dd;
+}	t_quadratic;
 
 t_rgb_int	ray_path(t_ray *ray, t_scene *scene, t_object **hit_object);
 float		hit_register(t_ray *restrict ray, t_scene *scene, t_object **hit_object);
@@ -26,5 +41,6 @@ int			hit_triangle(t_ray *ray, t_object *o, float *t);
 void		calc_bvh_bound(t_camera *cam, t_bound *bound, t_bvh_main *bvh, int mode);
 int			imax(int a, int b);
 int			imin(int a, int b);
+int			solve_quadratic(t_quadratic *q);
 
 #endif
