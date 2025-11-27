67b8028afa85030bab7adc7554c30bbbe477c1bf
pabellis <mail@bellissantpablo.fr>
2025-11-27 04:52:17 +0100

    feat: added material render and parser

--- src/loop.c
+++ src/loop.c
@@ -104,7 +104,6 @@ void	draw(t_img_data *img, t_camera *cam, t_data *data, int pixel_size)
 
 void	draw_individual(t_img_data *img, t_camera *cam, t_data *data, int pixel_size, int actual_pixel)
 {
-	t_ray		ray;
 	int			color;
 	t_vec2i		pixel;
 
@@ -118,8 +117,6 @@ void	draw_individual(t_img_data *img, t_camera *cam, t_data *data, int pixel_siz
 		{
 			cam->y_offset =	vec3_scale(cam->pixel_delta_v, (float)pixel.y);
 			recalc_camera_y(cam);
-			ray.pos = cam->pos;
-			ray.dir = vec3_normalize(vec3_sub(cam->pixel_center, cam->pos));
 			color = get_subsampling(vec2((float)pixel.x, (float)pixel.y), cam, SUB_PIXEL_QUANTITY, data);
 			ft_mlx_pixel_put(img, pixel, color);
 			pixel.y += pixel_size;
